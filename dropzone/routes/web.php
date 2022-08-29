ce573109cd35950b0b7d33453b7e73a00dac1cde
지승렬 <seungryeol156@gmail.com>
2022-08-29 15:41:35 +0900

    라우트 설정

--- dropzone/routes/web.php
+++ dropzone/routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/image', [ImageController::class, 'index'])->name('image.index');
-Route::get('/image/create', [ImageController::class, 'create'])->name('image.create');
-Route::post('/image/store', [ImageController::class, 'store'])->name('image.store');
-Route::delete('image/{image}', [ImageController::class, 'destroy'])->name('image.destroy');
-Route::post('/image/destroy', [ImageController::class, 'ajaxDestroy'])->name('image.ajaxDestroy');
+Route::get('/', [ImageController::class, 'index'])->name('image.index');
+Route::get('/create', [ImageController::class, 'create'])->name('image.create');
+Route::post('/store', [ImageController::class, 'store'])->name('image.store');
+Route::delete('{image}', [ImageController::class, 'destroy'])->name('image.destroy');
+Route::post('/destroy', [ImageController::class, 'ajaxDestroy'])->name('image.ajaxDestroy');
 
